eadeb201f418352d78641211388fe51cf0e45603
Patrick Wyatt <pat@codeofhonor.com>
2013-05-22 18:55:36 +0000

    Create truecrypt file for credentials

--- configure.sh
+++ configure.sh
@@ -21,16 +21,22 @@ function homebrew_install () {
 }
 
 function package_install () {
+  # already installed?
+  if which $1 &>/dev/null; then
+    echo $1 already installed
+    return
+  fi
+
   case "$OSTYPE" in
     darwin*)
-      for pkg in "$@"; do brew install "$pkg"; done
+      brew install "$1"
     ;;
 
     linux*)
       if which apt-get &>/dev/null ; then
-        sudo apt-get install -y "$@"
+        sudo apt-get install -y "$1"
       elif which yum &>/dev/null ; then
-        sudo yum install -y "$@"
+        sudo yum install -y "$1"
       else
         echo "Unknown Linux distro" && exit 1
       fi
@@ -63,8 +69,37 @@ function github_update_repo () {
   git_update_repo git://github.com/$user/$pkg.git "$dir"/$pkg
 }
 
+function install_vagrant_plugin () {
+  if ! (vagrant plugin list | grep $1 &>/dev/null) ; then
+    vagrant plugin install $1
+  fi
+}
+
+function get_template () {
+  \curl -LsS >$1 https://raw.github.com/webcoyote/aws-workstation-setup/master/templates/$2
+}
+
+function create_config_template () {
+  if [ ! -f ~/.credentials/$1 ]; then
+    get_template ~/.credentials/$1 $1
+  fi
+}
+
+
 # Ask for the password up front so the rest of the script just works
 sudo true
+
+# Ask for truecrypt password
+read -s -p "Enter TrueCrypt password for ~/.TrueCrypt/credentials.tc: " PASSWORD
+echo
+if [ ! -f ~/.TrueCrypt/credentials.tc ]; then
+  read -s -p "Please re-enter password: " PASSWORD2
+  echo
+  if [ "$PASSWORD" != "$PASSWORD2" ]; then
+    echo "Passwords don't match"
+    exit 1
+  fi
+fi
 
 # Install packages
 homebrew_install
@@ -78,17 +113,40 @@ github_update_repo opscode-cookbooks dmg ~/.cache/aws-prep/cookbooks
 github_update_repo opscode-cookbooks yum ~/.cache/aws-prep/cookbooks
 
 # Install soloist in system ruby
-sudo gem install soloist </dev/null
+(which soloist &>/dev/null) || (sudo gem install soloist </dev/null)
 
 # Configure soloist
 cd ~/.cache/aws-prep
-\curl -LsS > soloistrc https://raw.github.com/webcoyote/aws-workstation-setup/master/soloistrc
+get_template soloistrc soloistrc
 
 # Run soloist to install other required software, like Vagrant
 soloist
 
 # Install vagrant plugins
-vagrant plugin install vagrant-berkshelf
-vagrant plugin install vagrant-aws
+install_vagrant_plugin vagrant-berkshelf
+install_vagrant_plugin vagrant-aws
+
+# Create truecrypt drive for credentials
+if [ ! -f ~/.TrueCrypt/credentials.tc ]; then
+  echo "-"
+  echo "Creating truecrypt file; takes five minutes :("
+  echo "If you move your mouse really quickly it will only take ~30 seconds -- really!"
+  echo "-"
+  truecrypt-cmd --create --size=$((1024*1024)) --volume-type=normal         \
+    --encryption=AES-Twofish-Serpent --hash=RIPEMD-160 --filesystem=fat     \
+    -p "$PASSWORD" --keyfiles='' --random-source=/dev/random        \
+      ~/.TrueCrypt/credentials.tc
+fi
+
+# Unmount the credentials file in case it is mounted, then re-mount
+mkdir -p ~/.credentials
+truecrypt-cmd -d ~/.credentials &>/dev/null || true
+truecrypt-cmd --mount -p "$PASSWORD" --keyfiles='' \
+  --protect-hidden=no ~/.TrueCrypt/credentials.tc ~/.credentials
+
+# Configure AWS and Perforce
+create_config_template p4config
+create_config_template awsconfig.yml
 
 echo "Setup complete"
+
